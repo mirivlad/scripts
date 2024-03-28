6a6cb8f01f1be6bbe015ba863b305ed3d2717d5c
mirivlad <mirvtop@yandex.ru>
2024-03-28 22:57:52 +0800

    Change commet ion get_pc_temp.sh

--- get_pc_temp.sh
+++ get_pc_temp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# This script show current GPU temperature for Radeon. 
+# This script show current CPU,GPU, and NVME temperature. 
+# May be change for installed and showing by lm_sensors hardware 
 # Require installed lm_sensors
 GPUTEMP="$(sensors | grep -A 0 'edge' | cut -c15-22)"
 CPUTEMP="$(sensors | grep -A 0 'Package id' | cut -c16-23)"
